456aa97317e6d001e0b1a5a94498b77ef37bb6e8
Ivan Atias <ivan.d.atias@gmail.com>
2022-05-14 18:44:32 -0400

    add react toastify and global css hide scrollbar classes

--- postit__frontend/pages/_app.js
+++ postit__frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppContext } from "../context/context";
+import { ToastContainer } from "react-toastify";
 
 import "../styles/globals.css";
 
@@ -7,6 +8,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <AppContext>
       <Component {...pageProps} />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3500}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </AppContext>
   );
 }
