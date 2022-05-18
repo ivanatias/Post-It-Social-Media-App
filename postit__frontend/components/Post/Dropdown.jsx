41e90f0dc5f660cf74c04ea552de30e6ba399704
Ivan Atias <ivan.d.atias@gmail.com>
2022-05-17 22:13:05 -0400

    complete post Dropdown component

--- postit__frontend/components/Post/Dropdown.jsx
+++ postit__frontend/components/Post/Dropdown.jsx
@@ -15,9 +15,15 @@ const Dropdown = ({
   postedBy,
   postImage,
   postId,
+  saveOrUnsavePost,
+  saving,
+  unsaving,
+  alreadySaved,
+  refreshData,
 }) => {
   const { data: session } = useSession();
   const router = useRouter();
+
   return (
     <div className="absolute z-10 flex flex-col gap-2 p-3 bg-white rounded-md right-2 top-14">
       <a
@@ -29,12 +35,28 @@ const Dropdown = ({
         <AiOutlineDownload fontSize={16} />
         <p>Download</p>
       </a>
-      <div className="flex items-center gap-2 px-2 py-3 text-xs font-semibold transition duration-150 ease-in-out rounded-md shadow-md cursor-pointer lg:text-sm hover:bg-gray-100">
+      <button
+        className={`flex items-center gap-2 px-2 py-3 text-xs font-semibold transition duration-150 ease-in-out border-none rounded-md shadow-md outline-none cursor-pointer lg:text-sm hover:bg-gray-100 ${
+          alreadySaved?.length > 0 && "text-red-500"
+        }`}
+        onClick={() => {
+          saveOrUnsavePost(postId);
+          refreshData();
+        }}
+      >
         <AiOutlineSave fontSize={16} />
-        <p>Save</p>
-      </div>
-      <div
-        className="flex items-center gap-2 px-2 py-3 text-xs font-semibold transition duration-150 ease-in-out rounded-md shadow-md cursor-pointer lg:text-sm hover:bg-gray-100"
+        <p>
+          {saving
+            ? "Saving..."
+            : unsaving
+            ? "Unsaving"
+            : alreadySaved?.length > 0
+            ? "Saved"
+            : "Save"}
+        </p>
+      </button>
+      <button
+        className="flex items-center gap-2 px-2 py-3 text-xs font-semibold transition duration-150 ease-in-out border-none rounded-md shadow-md outline-none cursor-pointer lg:text-sm hover:bg-gray-100"
         onClick={() => {
           setDropdownOpen(false);
           router.push(`/post/${postId}`);
@@ -42,10 +64,10 @@ const Dropdown = ({
       >
         <AiOutlineEye fontSize={16} />
         <p>Post Details</p>
-      </div>
-      {session.user.uid === postedBy?._id && (
-        <div
-          className="flex items-center gap-2 px-2 py-3 text-xs font-semibold transition duration-150 ease-in-out rounded-md shadow-md cursor-pointer lg:text-sm hover:bg-gray-100"
+      </button>
+      {session?.user?.uid === postedBy?._id && (
+        <button
+          className="flex items-center gap-2 px-2 py-3 text-xs font-semibold transition duration-150 ease-in-out border-none rounded-md shadow-md outline-none cursor-pointer lg:text-sm hover:bg-gray-100"
           onClick={() => {
             setDropdownOpen(false);
             setOpenModal(true);
@@ -53,7 +75,7 @@ const Dropdown = ({
         >
           <AiOutlineDelete fontSize={16} />
           <p>Delete</p>
-        </div>
+        </button>
       )}
     </div>
   );
