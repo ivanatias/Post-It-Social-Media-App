1e460bfc2a16ae307a3eeaad5275ffcfdaa531b5
Ivan Atias <ivan.d.atias@gmail.com>
2022-05-26 17:29:05 -0400

    Modify 404 and 500 pages title

--- postit__frontend/pages/404.js
+++ postit__frontend/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
   return (
     <>
       <Head>
-        <title>PostIt App</title>
+        <title>PostIt App | 404 Not Found</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8"></meta>
         <link rel="icon" href="/Postit-logoicon.svg"></link>
