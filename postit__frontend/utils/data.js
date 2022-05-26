92a087c1473e517b8bca0ed4bbd7c697cff01b15
Ivan Atias <ivan.d.atias@gmail.com>
2022-05-26 17:17:36 -0400

    add new fetchers functions and queries

--- postit__frontend/utils/data.js
+++ postit__frontend/utils/data.js
@@ -60,3 +60,72 @@ export const postsQuery = () => {
 
   return query;
 };
+
+export const postQuery = (postId) => {
+  const query = `*[_type == "post" && _id == '${postId}'] {
+    image {
+      asset -> {
+        url
+      }
+    },
+    _id,
+    title,
+    description,
+    category,
+    postedBy -> {
+      _id,
+      userName,
+      image,
+      userTag,
+    },
+    saved[] {
+      _key,
+      postedBy -> {
+        _id,
+        userName,
+        image,
+        userTag
+      }
+     }, 
+    comments[] {
+      comment,
+      _key,
+      postedBy -> {
+        _id,
+        userName,
+        image,
+        userTag,
+      }
+    },
+  }`;
+
+  return query;
+};
+
+export const postsByCategoryQuery = (category, postId) => {
+  const query = `*[_type == "post" && category == '${category}' && _id != '${postId}' ] | order(_createdAt desc) {
+    image {
+      asset -> {
+        url
+      }
+    },
+    _id,
+    title,
+    postedBy -> {
+      _id,
+      userName,
+      image,
+      userTag,
+    },
+    saved[] {
+      _key,
+      postedBy -> {
+        _id,
+        userName,
+        image
+      }
+    },
+  }`;
+
+  return query;
+};
--- postit__frontend/utils/fetchers.js
+++ postit__frontend/utils/fetchers.js
@@ -1,9 +1,33 @@
-import { postsQuery } from "./data";
+import { postsQuery, postQuery, postsByCategoryQuery } from "./data";
 import axios from "axios";
 
 export const fetchAllPosts = async () => {
   try {
     const query = postsQuery();
+    const response = await axios.post("/api/posts/getPosts", {
+      posts: query,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchPost = async (postId) => {
+  try {
+    const query = postQuery(postId);
+    const response = await axios.post("/api/posts/getPost", {
+      post: query,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchPostsByCategory = async (category, postId) => {
+  try {
+    const query = postsByCategoryQuery(category, postId);
     const response = await axios.post("/api/posts/getPosts", {
       posts: query,
     });
