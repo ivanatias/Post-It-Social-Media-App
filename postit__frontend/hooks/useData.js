7bcc51bce6faa94260bf98907164d612a46bb1c4
Ivan Atias <ivan.d.atias@gmail.com>
2022-05-26 17:24:58 -0400

    useData hook refactor

--- postit__frontend/hooks/useData.js
+++ postit__frontend/hooks/useData.js
@@ -1,11 +1,17 @@
 import { useQuery } from "react-query";
+import { toast } from "react-toastify";
 
-export const useData = (queryKey, queryFn, queryFnParams = null) => {
+export const useData = (queryKey, queryFn, queryFnParam1, queryFnParam2) => {
   const { data, isFetching, refetch } = useQuery(
     queryKey,
-    () => queryFn(queryFnParams),
+    () => queryFn(queryFnParam1, queryFnParam2),
     {
       staleTime: 2.5 * 60 * 1000, //2.5 minutes
+      onError: (error) => {
+        toast.error(
+          `Couldn't establish connection due to error: ${error.message}`
+        );
+      },
     }
   );
 
