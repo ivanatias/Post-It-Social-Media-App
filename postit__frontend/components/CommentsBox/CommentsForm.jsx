46394733030543feb834890de886ceab0cb3d1bd
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-18 21:16:08 -0400

    refactor(CommentsBox): abstract comments form

---
+++ postit__frontend/components/CommentsBox/CommentsForm.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { UserAvatar } from "../../components";
+
+const CommentsForm = ({
+  onSubmit,
+  onChange,
+  commentInput,
+  addingComment,
+  avatarUrl,
+}) => (
+  <form
+    className="flex items-center justify-between w-full"
+    onSubmit={onSubmit}
+  >
+    <div className="flex items-center flex-1 gap-3 mt-2">
+      {avatarUrl && <UserAvatar avatarUrl={avatarUrl} />}
+      <textarea
+        placeholder="Add a comment"
+        value={commentInput || ""}
+        onChange={onChange}
+        rows={2}
+        className="text-white border-2 min-h-[60px] max-h-[100px] flex items-center w-full px-4 py-3 text-xs bg-transparent border-gray-600 rounded-lg outline-none 2xl:text-base"
+      />
+    </div>
+    <button
+      type="submit"
+      className="px-4 py-3 text-xs font-bold text-red-500 transition duration-150 ease-in-out border-none outline-none 2xl:text-base hover:text-red-600 disabled:opacity-40"
+      disabled={addingComment || !commentInput}
+    >
+      {addingComment ? "Adding..." : "Comment"}
+    </button>
+  </form>
+);
+
+export default CommentsForm;
--- postit__frontend/components/CommentsBox/index.jsx
+++ postit__frontend/components/CommentsBox/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
 import { Loading } from "../../components";
 import CommentsContainer from "./CommentsContainer";
 import Comment from "./Comment";
+import CommentsForm from "./CommentsForm";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import axios from "axios";
 
@@ -13,6 +14,10 @@ const CommentsBox = ({ comments, refresh, isFetching }) => {
   const { data: session } = useSession();
   const router = useRouter();
   const { id } = router.query;
+
+  const handleCommentChange = (e) => {
+    setCommentInput(e.target.value);
+  };
 
   const addComment = (e) => {
     e.preventDefault();
@@ -73,33 +78,13 @@ const CommentsBox = ({ comments, refresh, isFetching }) => {
           </span>
         )}
       </CommentsContainer>
-
-      <form
-        className="flex items-center justify-between w-full"
+      <CommentsForm
         onSubmit={addComment}
-      >
-        <div className="flex items-center flex-1 gap-3 mt-2">
-          <img
-            src={session?.user?.image}
-            alt="User Avatar"
-            className="object-cover w-8 h-8 rounded-full"
-          />
-          <textarea
-            placeholder="Add a comment"
-            value={commentInput || ""}
-            onChange={(e) => setCommentInput(e.target.value)}
-            rows={2}
-            className="text-white border-2 min-h-[60px] max-h-[100px] flex items-center w-full px-4 py-3 text-xs bg-transparent border-gray-600 rounded-lg outline-none 2xl:text-base"
-          />
-        </div>
-        <button
-          type="submit"
-          className="px-4 py-3 text-xs font-bold text-red-500 transition duration-150 ease-in-out border-none outline-none 2xl:text-base hover:text-red-600 disabled:opacity-40"
-          disabled={addingComment || !commentInput}
-        >
-          {addingComment ? "Adding..." : "Comment"}
-        </button>
-      </form>
+        onChange={handleCommentChange}
+        commentInput={commentInput}
+        addingComment={addingComment}
+        avatarUrl={session?.user?.image}
+      />
       {isFetching && <Loading />}
     </>
   );
