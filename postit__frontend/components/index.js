03b8da3687f88fc590f2cc70469304cceac1e025
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-18 23:43:56 -0400

    refactor: move ConfirmModal to root components folder

--- postit__frontend/components/index.js
+++ postit__frontend/components/index.js
@@ -3,6 +3,7 @@ export { default as Sidebar } from "./Sidebar";
 export { default as Layout } from "./Layout";
 export { default as Loading } from "./Loading";
 export { default as Post } from "./Post";
+export { default as ConfirmModal } from "./ConfirmModal";
 export { default as Posts } from "./Posts";
 export { default as CommentsBox } from "./CommentsBox";
 export { default as SavedByBox } from "./SavedByBox";
