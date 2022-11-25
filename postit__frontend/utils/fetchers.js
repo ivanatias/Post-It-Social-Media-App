684677bf219126382d0bf5347adba9b8fbdc2ccc
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-24 21:18:14 -0400

    refactor(fetchers): remove try catch blocks so errors are automatically catched by react-query

--- postit__frontend/utils/fetchers.js
+++ postit__frontend/utils/fetchers.js
@@ -10,85 +10,57 @@ import {
 import axios from "axios";
 
 export const fetchAllPosts = async () => {
-  try {
-    const query = postsQuery();
-    const response = await axios.post("/api/posts/getPosts", {
-      posts: query,
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const query = postsQuery();
+  const response = await axios.post("/api/posts/getPosts", {
+    posts: query,
+  });
+  return response.data;
 };
 
 export const fetchPost = async (postId) => {
-  try {
-    const query = postQuery(postId);
-    const response = await axios.post("/api/posts/getPost", {
-      post: query,
-    });
-    return response.data[0];
-  } catch (error) {
-    console.log(error);
-  }
+  const query = postQuery(postId);
+  const response = await axios.post("/api/posts/getPost", {
+    post: query,
+  });
+  return response.data[0];
 };
 
 export const fetchPostsByCategory = async (category, postId) => {
-  try {
-    const query = postsByCategoryQuery(category, postId);
-    const response = await axios.post("/api/posts/getPosts", {
-      posts: query,
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const query = postsByCategoryQuery(category, postId);
+  const response = await axios.post("/api/posts/getPosts", {
+    posts: query,
+  });
+  return response.data;
 };
 
 export const fetchUser = async (userId) => {
-  try {
-    const query = userQuery(userId);
-    const response = await axios.post("/api/users/getUser", {
-      user: query,
-    });
-    return response.data[0];
-  } catch (error) {
-    console.log(error);
-  }
+  const query = userQuery(userId);
+  const response = await axios.post("/api/users/getUser", {
+    user: query,
+  });
+  return response.data[0];
 };
 
 export const fetchPostsByUser = async (userId) => {
-  try {
-    const query = postsByUserQuery(userId);
-    const response = await axios.post("/api/posts/getPosts", {
-      posts: query,
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const query = postsByUserQuery(userId);
+  const response = await axios.post("/api/posts/getPosts", {
+    posts: query,
+  });
+  return response.data;
 };
 
 export const fetchPostsSavedByUser = async (userId) => {
-  try {
-    const query = postsSavedByUserQuery(userId);
-    const response = await axios.post("/api/posts/getPosts", {
-      posts: query,
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const query = postsSavedByUserQuery(userId);
+  const response = await axios.post("/api/posts/getPosts", {
+    posts: query,
+  });
+  return response.data;
 };
 
 export const fetchSearchedPosts = async (searchTerm) => {
-  try {
-    const query = searchPostQuery(searchTerm.toLowerCase());
-    const response = await axios.post("/api/posts/getPosts", {
-      posts: query,
-    });
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const query = searchPostQuery(searchTerm.toLowerCase());
+  const response = await axios.post("/api/posts/getPosts", {
+    posts: query,
+  });
+  return response.data;
 };
